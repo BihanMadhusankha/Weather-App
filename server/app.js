ba5f6143cfc0a2e94fe08eebfa531ef896ee34c6
BihanMadhusankha <ellawalagemadhusankha02@gmail.com>
2024-03-16 17:09:16 +0530

    Eddit the map

--- server/app.js
+++ server/app.js
@@ -1,141 +1,4 @@
 
-document.getElementById("searchTxt").addEventListener("keypress", async (event) => {
-    if (event.key === "Enter") {
-
-        try {
-            const searchVal = document.getElementById("searchTxt").value;
-
-            let reop = {
-                method: 'POST'
-            };
-            fetch(`https://api.weatherapi.com/v1/forecast.json?key=f1850d9ec02649c4b0a84749240403&q=${searchVal}&days=7`, reop)
-                .then(response => response.json())
-                .then(data => {
-
-                    //location start
-                    document.getElementById("temp").innerHTML = data["current"]["temp_c"] + " °C";
-                    document.getElementById("location").innerHTML = data["location"]["name"];
-                    document.getElementById("feelslike").innerHTML = `Feels like ${data.current.feelslike_c} °C`;
-                    document.getElementById("wetherimg").src = data["current"]["condition"]["icon"];
-                    //location end
-
-
-                    //Today'highlight start
-                    document.getElementById("uv").innerHTML = data["current"]["uv"];
-                    document.getElementById("chanceofrain").innerHTML = `${data.forecast.forecastday[0].day.daily_chance_of_rain} %`;
-                    document.getElementById("humidity").innerHTML = data["current"]["humidity"] + "%";
-                    document.getElementById("wind").innerHTML = `Wind: ${data["current"]["wind_kph"]} km/h`;
-                    //Today'highlight end
-
-                    //Today sunset start
-
-                    for (var i = 7; i < 22; i += 7) {
-                        document.getElementById(`wetherImg${i}`).innerHTML = `${data.forecast.forecastday[0].hour[i].condition.icon}`
-                        document.getElementById(`tempT${i}`).innerHTML = `${data.forecast.forecastday[0].hour[i].temp_c} °C`;
-
-                    }
-
-                    document.getElementById("sunrise").innerHTML = `${data.forecast.forecastday[0].astro.sunrise}`
-                    document.getElementById("sunset").innerHTML = `${data.forecast.forecastday[0].astro.sunset}`
-                    document.getElementById("dayLength").innerHTML = `${data.location.localtime}`
-                    //Today sunset end
-
-                    //future days start
-                    const startDate = new Date(`${data.forecast.forecastday[0].date}`);
-                    let currentDay = new Date(startDate);
-
-                    for (let i = 0; i < 7; i++) {
-                        //hethuw blann toISOString, split
-                        const formattedDate = currentDay.toISOString().split('T')[0];
-
-                        fetch(`https://api.weatherapi.com/v1/forecast.json?key=f1850d9ec02649c4b0a84749240403&q=${searchVal}&days=7&dt=${formattedDate}&aqi=homagama&alerts=yes`)
-                            .then(response => response.json())
-                            .then(data => {
-                                document.getElementById(`day${i + 1}`).innerHTML = `${data.forecast.forecastday[0].date}`
-                                document.getElementById(`day${i + 1}Temp`).innerHTML = `${data.forecast.forecastday[0].day.avgtemp_c} °C`;
-                                document.getElementById(`day${i + 1}Img`).src = `${data.forecast.forecastday[0].day.condition.icon}`;
-                            })
-                            .catch(error => {
-                                console.error("Error:", error);
-                            });
-
-                        currentDay.setDate(currentDay.getDate() + 1);
-                    }
-                    //End feuture days
-
-                    //Start past days
-                    const startDay = new Date(`${data.forecast.forecastday[0].date}`);
-                    let currentDays = new Date(startDay);
-
-                    for (let i = 3; i > 0; i--) {
-
-                        const formattedDate = currentDays.toISOString().split('T')[0];
-
-                        fetch(`http://api.weatherapi.com/v1/history.json?key=f1850d9ec02649c4b0a84749240403&q=${searchVal}&dt=${formattedDate}&aqi=homagama&alerts=yes`)
-                            .then(response => response.json())
-                            .then(data => {
-                                document.getElementById(`Day${i}`).innerHTML = `${data.forecast.forecastday[0].date}`;
-                                document.getElementById(`Day${i}Temp`).innerHTML = `${data.forecast.forecastday[0].day.avgtemp_c} °C`;
-                                document.getElementById(`day${i}AImg`).src = `${data.forecast.forecastday[0].day.condition.icon}`;
-                            })
-                            .catch(error => {
-                                console.error("Error:", error);
-                            });
-
-                        currentDays.setDate(currentDays.getDate() - 1);
-                    }
-
-                    //End past days
-
-                    let map; // Declare a variable to hold the map instance
-
-                    document.getElementById("searchBtn").addEventListener("click", async () => {
-                        try {
-                            const searchVal = document.getElementById("searchTxt").value;
-                            const { latitude, longitude } = await fetchLocationData(searchVal);
-
-                            // Check if map is already initialized
-                            if (!map) {
-                                // If map is not initialized, create a new one
-                                map = L.map('map').setView([latitude, longitude], 13);
-
-                                // Add the OpenStreetMap tile layer
-                                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                }).addTo(map);
-                            } else {
-                                // If map is already initialized, just update its view
-                                map.setView([latitude, longitude], 13);
-                            }
-
-                            // Add a marker to the map
-                            L.marker([latitude, longitude]).addTo(map)
-                                .bindPopup(`Location: ${searchVal}`)
-                                .openPopup();
-                        } catch (error) {
-                            console.error('Error during fetch:', error);
-                            // Handle errors
-                        }
-                    });
-
-                    async function fetchLocationData(searchVal) {
-                        try {
-                            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=f1850d9ec02649c4b0a84749240403&q=${searchVal}&days=7`);
-                            const data = await response.json();
-                            return { latitude: data.location.lat, longitude: data.location.lon };
-                        } catch (error) {
-                            console.error('Error fetching location data:', error);
-                            throw error; // Rethrow the error to handle it elsewhere if needed
-                        }
-                    }
-                })
-        } catch (error) {
-            console.error('Error during fetch:', error);
-        }
-    }
-});
-
-
 document.getElementById("searchBtn").addEventListener("click", async () => {
     try {
         const searchVal = document.getElementById("searchTxt").value;
@@ -273,53 +136,8 @@ document.getElementById("searchBtn").addEventListener("click", async () => {
                     currentDays.setDate(currentDays.getDate() - 1);
                 }
 
-                let map; // Declare a variable to hold the map instance
-
-                document.getElementById("searchBtn").addEventListener("click", async () => {
-                    try {
-                        const searchVal = document.getElementById("searchTxt").value;
-                        const { latitude, longitude } = await fetchLocationData(searchVal);
-
-                        // Check if map is already initialized
-                        if (!map) {
-                            // If map is not initialized, create a new one
-                           
-                            map = L.map('map').setView([latitude, longitude], 13);
-
-                            // Add the OpenStreetMap tile layer
-                            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            }).addTo(map);
-                        } else {
-                            // If map is already initialized, just update its view
-                            map.setView([latitude, longitude], 13);
-                        }
-
-                        // Add a marker to the map
-                        L.marker([latitude, longitude]).addTo(map)
-                            .bindPopup(`Location: ${searchVal}`)
-                            .openPopup();
-                    } catch (error) {
-                        console.error('Error during fetch:', error);
-                        // Handle errors
-                    }
-                });
-
-                async function fetchLocationData(searchVal) {
-                    try {
-                        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=f1850d9ec02649c4b0a84749240403&q=${searchVal}&days=7`);
-                        const data = await response.json();
-                        return { latitude: data.location.lat, longitude: data.location.lon };
-                    } catch (error) {
-                        console.error('Error fetching location data:', error);
-                        throw error; // Rethrow the error to handle it elsewhere if needed
-                    }
-                }
-
-
 
             })
-
 
     } catch (error) {
         console.error('Error during fetch:', error);
@@ -444,6 +262,7 @@ function currentLocation() {
                             document.getElementById(`day${i + 1}`).innerHTML = `${data.forecast.forecastday[0].date}`
                             document.getElementById(`day${i + 1}Temp`).innerHTML = `${data.forecast.forecastday[0].day.avgtemp_f} F`;
                             document.getElementById(`day${i + 1}Img`).src = `${data.forecast.forecastday[0].day.condition.icon}`;
+                            document.getElementById(`day${i + 1}text`).innerHTML = `${data.forecast.forecastday[0].day.condition.text} mph`;
 
                         });
 
@@ -451,6 +270,8 @@ function currentLocation() {
                             document.getElementById(`day${i + 1}`).innerHTML = `${data.forecast.forecastday[0].date}`
                             document.getElementById(`day${i + 1}Temp`).innerHTML = `${data.forecast.forecastday[0].day.avgtemp_c} °C`;
                             document.getElementById(`day${i + 1}Img`).src = `${data.forecast.forecastday[0].day.condition.icon}`;
+                            document.getElementById(`day${i + 1}text`).innerHTML = `${data.forecast.forecastday[0].day.condition.text} mph`;
+
 
                         });
                         document.getElementById(`day${i + 1}`).innerHTML = `${data.forecast.forecastday[0].date}`
@@ -498,19 +319,6 @@ function currentLocation() {
 
                 currentDays.setDate(currentDays.getDate() - 1);
             }
-
-            var map = L.map('map').setView([80.0032, 6.8433], 13);
-
-            // Add the OpenStreetMap tile layer
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '© OpenStreetMap contributors'
-            }).addTo(map);
-
-            // Add a marker to the map
-            var marker = L.marker([80.0032, 6.8433]).addTo(map);
-
-            // Add a popup to the marker
-            marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
         })
 
@@ -604,4 +412,56 @@ iconMoon.addEventListener('click', () => {
     }
 });
 
+let map; // Declare a variable to hold the map instance
 
+function initializeMap(latitude, longitude, pin) {
+    if (!map) {
+        map = L.map('map').setView([latitude, longitude], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    } else {
+        map.setView([latitude, longitude], 13);
+    }
+
+    // Add a marker to the map
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup(`Location: ${pin}`)
+        .openPopup();
+}
+
+// Event listener for search button
+document.getElementById("searchBtn").addEventListener("click", async () => {
+    try {
+        const searchVal = document.getElementById("searchTxt").value;
+        const { latitude, longitude } = await fetchLocationData(searchVal);
+        initializeMap(latitude, longitude, searchVal);
+    } catch (error) {
+        console.error('Error during fetch:', error);
+        // Handle errors
+    }
+});
+
+async function currentMap(currentLocation) {
+    try {
+        const { latitude, longitude } = await fetchLocationData(currentLocation);
+        initializeMap(latitude, longitude, currentLocation);
+    } catch (error) {
+        console.error('Error during fetch:', error);
+        // Handle errors
+    }
+}
+
+async function fetchLocationData(location) {
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=f1850d9ec02649c4b0a84749240403&q=${location}&days=7`);
+        const data = await response.json();
+        return { latitude: data.location.lat, longitude: data.location.lon };
+    } catch (error) {
+        console.error('Error fetching location data:', error);
+        throw error; // Rethrow the error to handle it elsewhere if needed
+    }
+}
+
+currentMap('Homagama')
